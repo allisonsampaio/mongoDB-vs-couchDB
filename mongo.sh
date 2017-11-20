5effc46e4946f39b29efcf9081bf6f5ced72cf91
MGoulart <31328666+ezeq3@users.noreply.github.com>
2017-11-20 11:37:03 -0200

    insert
    ainda falta anexar pdf

--- mongo.sh
+++ mongo.sh
@@ -1,1 +1,67 @@
 # Script para criação de um banco de dados no MongoDB
+
+mongo
+
+db.poscomp.insert ( {
+"name":  "prova1"
+"year": "2002"})
+
+db.poscomp.insert ( {
+"name":  "prova2"
+"year": "2003"})
+
+db.poscomp.insert ( {
+"name":  "prova3"
+"year": "2004"})
+
+db.poscomp.insert ( {
+"name":  "prova4"
+"year": "2005"})
+
+db.poscomp.insert ( {
+"name":  "prova5"
+"year": "2006"})
+
+db.poscomp.insert ( {
+"name":  "prova7"
+"year": "2007"})
+
+db.poscomp.insert ( {
+"name":  "prova8"
+"year": "2008"})
+
+db.poscomp.insert ( {
+"name":  "prova9"
+"year": "2009"})
+
+db.poscomp.insert ( {
+"name":  "prova10"
+"year": "2010"})
+
+db.poscomp.insert ( {
+"name":  "prova11"
+"year": "2011"})
+
+db.poscomp.insert ( {
+"name":  "prova12"
+"year": "2012"})
+
+db.poscomp.insert ( {
+"name":  "prova13"
+"year": "2013"})
+
+db.poscomp.insert ( {
+"name":  "prova14"
+"year": "2014"})
+
+db.poscomp.insert ( {
+"name":  "prova15"
+"year": "2015"})
+
+db.poscomp.insert ( {
+"name":  "prova16"
+"year": "2016"})
+
+db.poscomp.find() {
+}
+
